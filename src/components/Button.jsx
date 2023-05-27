12bb5398a616f446dc7c58121b98939aa4166f58
romazpua <roma.zp.ua@gmail.com>
2023-05-27 15:24:46 +0200

    add react-router

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,10 +3,11 @@ import classNames from 'classnames';
 
 const Button = ( { outline, children, className, onClick } ) => {
     return (
-        <button onClick={ onClick }
-                className={ classNames( 'button', className, {
-                    'button--outline': outline
-                } ) }>
+        <button
+            onClick={ onClick }
+            className={ classNames( 'button', className, {
+                'button--outline': outline
+            } ) }>
             { children }
         </button>
     )
