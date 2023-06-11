bc4d8cc1bbd6a94f84f8e1f7ba51efe5c35bbc91
romazpua <roma.zp.ua@gmail.com>
2023-06-11 12:34:36 +0200

    finish

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import Button from './Button';
 
-const CartItem = ( { id, name, size, type, imageUrl, totalPrice, totalCount, onRemove } ) => {
+const CartItem = ( { id, name, size, type, imageUrl, totalPrice, totalCount, onRemove, onPlus, onMinus } ) => {
 
-    const handleRemoveClick = ( ) => {
-        onRemove( id )
-    }
+    const handleRemoveClick = () => onRemove( id )
+    const handlePlusItem = () => onPlus( id )
+    const handleMinusItem = () => onMinus( id )
 
     return (
         <div className="cart__item">
@@ -21,7 +21,8 @@ const CartItem = ( { id, name, size, type, imageUrl, totalPrice, totalCount, onR
                 <p>{ type } тесто, { size } см.</p>
             </div>
             <div className="cart__item-count">
-                <div className="button button--outline button--circle cart__item-count-minus">
+                <div onClick={ handleMinusItem }
+                     className="button button--outline button--circle cart__item-count-minus">
                     <svg width="10" height="10" viewBox="0 0 10 10" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
@@ -31,10 +32,10 @@ const CartItem = ( { id, name, size, type, imageUrl, totalPrice, totalCount, onR
                             d="M5.75998 5.92001L3.83998 5.92001L0.959977 5.92001C0.429817 5.92001 -2.29533e-05 5.49017 -2.29301e-05 4.96001C-2.2907e-05 4.42985 0.429817 4.00001 0.959977 4.00001L3.83998 4L5.75998 4.00001L8.63998 4.00001C9.17014 4.00001 9.59998 4.42985 9.59998 4.96001C9.59998 5.49017 9.17014 5.92001 8.63998 5.92001L5.75998 5.92001Z"
                             fill="#EB5A1E"/>
                     </svg>
-
                 </div>
                 <b>{ totalCount }</b>
-                <div className="button button--outline button--circle cart__item-count-plus">
+                <div onClick={ handlePlusItem }
+                     className="button button--outline button--circle cart__item-count-plus">
                     <svg width="10" height="10" viewBox="0 0 10 10" fill="none"
                          xmlns="http://www.w3.org/2000/svg">
                         <path
--- src/redux/reducers/cart.js
+++ src/redux/reducers/cart.js
@@ -51,6 +51,50 @@ const cart = ( state = initialState, action ) => {
         case 'CLEAR_CART':
             return { items: {}, totalPrice: 0, totalCount: 0 }
 
+        case 'PLUS_CART_ITEM': {
+            const newObjItems = [
+                ...state.items[ action.payload ].items,
+                state.items[ action.payload ].items[ 0 ]
+            ]
+            const newItems = {
+                ...state.items,
+                [ action.payload ]: {
+                    items: newObjItems,
+                    totalPrice: getTotalPrice( newObjItems )
+                }
+            }
+            const totalCount = getTotalSum( newItems, 'items.length' )
+            const totalPrice = getTotalSum( newItems, 'totalPrice' )
+            return {
+                ...state,
+                items: newItems,
+                totalCount,
+                totalPrice
+            }
+        }
+
+        case 'MINUS_CART_ITEM': {
+            const oldItems = state.items[ action.payload ].items
+            const newObjItems = oldItems.length > 1 ? oldItems.slice( 1 ) : oldItems
+
+            const newItems = {
+                ...state.items,
+                [ action.payload ]: {
+                    items: newObjItems,
+                    totalPrice: getTotalPrice( newObjItems )
+                }
+            }
+
+            const totalCount = getTotalSum( newItems, 'items.length' )
+            const totalPrice = getTotalSum( newItems, 'totalPrice' )
+            return {
+                ...state,
+                items: newItems,
+                totalCount,
+                totalPrice
+            }
+        }
+
         case 'REMOVE_CART_ITEM':
             const newItems = JSON.parse( JSON.stringify( state.items ) )
             const currentTotalPrice = newItems[ action.payload ].totalPrice
